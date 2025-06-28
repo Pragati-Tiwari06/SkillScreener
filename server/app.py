30d91dc08c5c7ac00e367de56b4d7c5722e517ac
pragati-Tiwari06 <pragatitiwari0610@gmail.com>
2025-06-29 00:49:41 +0530

    change

--- server/app.py
+++ server/app.py
@@ -30,5 +30,6 @@ def analyze_resume():
         return jsonify({'error': 'Internal Server Error'}), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
+    app.run(debug=True)
+
 
